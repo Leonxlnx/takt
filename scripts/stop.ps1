b52f2ce9723500fe474feece241af3ef5fe3a725
Leonxlnx <lexn.lin8@gmail.com>
2026-04-24 16:33:26 +0200

    Stop legacy background process

--- scripts/stop.ps1
+++ scripts/stop.ps1
@@ -1,3 +1,4 @@
 $ErrorActionPreference = "Stop"
 
 Get-Process -Name "keyme" -ErrorAction SilentlyContinue | Stop-Process -Force
+Get-Process -Name "keeby_windows" -ErrorAction SilentlyContinue | Stop-Process -Force
